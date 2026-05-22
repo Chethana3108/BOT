61c8bdea590aabc0ac9f8fdf8813464b4670cf25
Chethana3108 <chethana@biztechnosys.com>
2026-05-22 11:30:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app import app as recommendation_app
 from web import app as knowledge_app
-
+from web import startup_event
 
 main_app = FastAPI(
     title="Honda AI Unified Server"
 )
 
+# ==============================
+# CORS
+# ==============================
 
-main_app.mount("/recommendation", recommendation_app)
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-main_app.mount("/knowledge", knowledge_app)
+# ==============================
+# MOUNT APPS
+# ==============================
 
+main_app.mount("/chat", recommendation_app)
+
+main_app.mount("/knowledge_chat", knowledge_app)
+
+startup_event()
 
 @main_app.get("/")
 def home():
     return {
         "status": "Unified Honda AI Server Running"
     }
-    
-    
-    
-    uvicorn main:main_app --host 0.0.0.0 --port 8000
